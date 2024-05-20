c67d5b55edd306a8ef593e2173efe80ed0aeb299
Krekno <yigiterenkarabas@gmail.com>
2024-05-20 21:04:18 +0300

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let inputString = input.toLowerCase().replace(/[ ,.!?;:'"-]/g, '');
+    let reverseString = inputString.split('').reverse().join('');
+    return inputString === reverseString;
 };
 
 // Do not edit below this line
