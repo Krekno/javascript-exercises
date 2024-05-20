407128b01f0d5d78984160e8cb94af909d71d348
Krekno <yigiterenkarabas@gmail.com>
2024-05-20 21:16:00 +0300

    11 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = parseInt(x);
+    if (x < 0) return "OOPS";
+    arr = [0, 1];
+    for (let i = 2; i <= x; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr[x];
 };
 
 // Do not edit below this line
